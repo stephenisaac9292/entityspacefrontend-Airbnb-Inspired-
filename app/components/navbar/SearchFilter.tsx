d2c73a11982ef1733175591432134a42e76f9f71
Stephen Isaac <stephenisaac9292@gmail.com>
2025-06-24 12:00:51 +0100

    search button Init 1/2

--- app/components/navbar/SearchFilter.tsx
+++ app/components/navbar/SearchFilter.tsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 const SearchFilter = () => {
   return (
-<div className="h-[64px] w-max flex flex-row justify-between 
+<div className="relative h-[64px] w-max flex flex-row justify-between 
                 border border-gray-200 rounded-full ">
     {/* sub divisions */}
 
     {/* 1 */}
-    <div className='flex flex-col justify-center w-full h-full
+    <div className='flex flex-col justify-center h-full
                     px-8 box-border rounded-full hover:bg-gray-100 '
     >
         <p className='text-xs text-entityspace font-semibold'>Where</p>
-        <p className='text-sm  font-semibold'>Wanted location</p>
+        <p className='text-sm font-semibold'>Wanted location</p>
     </div>
 
     {/* 2 */}
-    <div className='flex flex-col justify-center w-full h-full
+    <div className='flex flex-col justify-center h-full
                     px-8 box-border rounded-full hover:bg-gray-100 '
     >
         <p className='text-xs text-entityspace font-semibold'>Check-in</p>
@@ -23,7 +23,7 @@ const SearchFilter = () => {
     </div>
 
     {/* 3 */}
-    <div className='flex flex-col justify-center w-full h-full
+    <div className='flex flex-col justify-center h-full
                     px-8 box-border rounded-full hover:bg-gray-100 '
     >
         <p className='text-xs text-entityspace font-semibold'>Check-out</p>
@@ -31,16 +31,24 @@ const SearchFilter = () => {
     </div>
 
     {/* 4 */}
-    <div className='flex flex-col justify-center w-full h-full
-                    px-8 box-border rounded-full hover:bg-gray-100 '
+    <div className='flex flex-col justify-center h-full
+                    px-8 pr-14 box-border rounded-full hover:bg-gray-100 '
     >
         <p className='text-xs text-entityspace font-semibold'>Call-us</p>
-        <p className='text-sm  font-semibold'>Rating</p>
+        <p className='text-sm  font-semibold'>Rating: please 5 star</p>
+
     </div>
+    <div className="absolute right-2 top-1/2 -translate-y-1/2 
+                    w-10 h-10 bg-entityspace text-white rounded-full 
+                      flex items-center justify-center 
+                      hover:bg-red-600 cursor-pointer transition">
+        
+      </div>
+   
+    
 </div>
   )
 }
 
 export default SearchFilter
-
 
