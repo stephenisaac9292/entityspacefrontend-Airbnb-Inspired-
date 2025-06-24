f310ad0e89a221e3d069a9044c21aa10e455c3ca
Stephen Isaac <stephenisaac9292@gmail.com>
2025-06-24 18:40:40 +0100

    break

--- app/components/navbar/SearchFilter.tsx
+++ app/components/navbar/SearchFilter.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const SearchFilter = () => {
   return (
-<div className="relative h-[50px] w-max flex flex-row justify-between 
-                border-2 border-gray-200 mt-5 rounded-full 
+<div className="relative h-[65px] w-max flex flex-row justify-between 
+                border border-gray-300 mt-5 rounded-full 
                 shadow-[0px_5px_15px_2px_#E5E4E2]">
     {/* sub divisions */}
 
@@ -11,36 +11,36 @@ const SearchFilter = () => {
     <div className='flex flex-col justify-center h-full
                     px-8 box-border rounded-full hover:bg-gray-200 '
     >
-        <p className='text-xs font-semibold'>Where</p>
-        <p className='text-sm text-gray-600 '>Search destinations</p>
+        <p className='text-xs font-semibold text-gray-900 font-sans'>Where</p>
+        <p className='text-sm text-gray-600 font-sans'>Search destinations</p>
     </div>
 
     {/* 2 */}
     <div className='flex flex-col justify-center h-full
                     px-8 box-border rounded-full hover:bg-gray-200 '
     >
-        <p className='text-xs font-semibold'>Where</p>
-        <p className='text-sm text-gray-600 '>Search destinations</p>
+        <p className='text-xs font-semibold'>Check in</p>
+        <p className='text-sm text-gray-600 '>Add dates</p>
     </div>
 
     {/* 3 */}
     <div className='flex flex-col justify-center h-full
                     px-8 box-border rounded-full hover:bg-gray-200 '
     >
-        <p className='text-xs font-semibold'>Where</p>
-        <p className='text-sm text-gray-600 '>Search destinations</p>
+        <p className='text-xs font-semibold'>Chech out</p>
+        <p className='text-sm text-gray-600 '>Add dates</p>
     </div>
 
     {/* 4 */}
     <div className='flex flex-col justify-center h-full
-                    px-8 pr-14 box-border rounded-full hover:bg-gray-200 '
+                    px-8 pr-18 box-border rounded-full hover:bg-gray-200 '
     >
-        <p className='text-xs font-semibold'>Where</p>
-        <p className='text-sm text-gray-600 '>Search destinations</p>
+        <p className='text-xs font-semibold'>Who</p>
+        <p className='text-sm text-gray-600 '>Add guests</p>
 
     </div>
     <div className="absolute right-2 top-1/2 -translate-y-1/2 
-                    w-10 h-10 bg-entityspace text-white rounded-full 
+                    w-12 h-12 bg-entityspace text-white rounded-full 
                       flex items-center justify-center 
                       hover:bg-red-600 cursor-pointer transition">
         
